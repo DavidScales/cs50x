6ec5fc5dc0c9be692c1eec7d00bd7df83e117656
David Scales <davidsscales@gmail.com>
2018-01-08 03:25:02 +0000

    feat(pset1): credit - validate card checksum

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int leading_digits(long long card, int num_leading_digits);
+int digit_at_position(long long num, int pos);
 
 int main(void)
 {
@@ -11,29 +11,7 @@ int main(void)
     long long card = get_long_long("%s", prompt);
 
     // validate positive non-zero
-    if (card <= 0) {
-        printf("INVALID\n");
-        return 0;
-    }
-
-    // validate length & starting digits
-    int total_num_digits = floor(log10(llabs(card))) + 1;
-    int leading = leading_digits(card, 2);
-    string type;
-
-    if (total_num_digits == 15 && (leading == 34 || leading == 37))
-    {
-        type ="AMEX\n";
-    }
-    else if (total_num_digits == 16 && (leading >= 51 && leading <= 55))
-    {
-        type ="MASTERCARD\n";
-    }
-    else if ((total_num_digits == 16 || total_num_digits == 13) && (leading >= 40 && leading <= 49))
-    {
-        type ="VISA\n";
-    }
-    else
+    if (card <= 0)
     {
         printf("INVALID\n");
         return 0;
@@ -41,19 +19,71 @@ int main(void)
 
     // validate checksum
 
-    // print status
+    int total_num_digits = floor(log10(llabs(card))) + 1;
+    int sum = 0;
+    int doubled;
 
-    // return 0;
+    // for every other card digit, starting second from the last
+    for (int i = 1; i < total_num_digits; i += 2)
+    {
+        // double the digit
+        doubled = digit_at_position(card, i) * 2;
+
+        // if the product has two digits, add both digits to the sum
+        if (doubled >= 10)
+        {
+            sum += doubled / 10 + doubled % 10;
+        }
+        // otherwise add the single digit to the sum
+        else
+        {
+            sum += doubled;
+        }
+    }
+
+    // for the remaining card digits
+    for (int i = 0; i < total_num_digits; i += 2)
+    {
+        // add digit to the sum
+        sum += digit_at_position(card, i);
+    }
+
+    // reject if the last digit of the sum is not zero
+    if (sum % 10 != 0)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    // validate length & starting digits (type)
+
+    int first_digit = digit_at_position(card, total_num_digits - 1);
+    int second_digit = digit_at_position(card, total_num_digits - 2);
+    int leading = first_digit * 10 + second_digit;
+
+    if (total_num_digits == 15 && (leading == 34 || leading == 37))
+    {
+        printf("AMEX\n");
+    }
+    else if (total_num_digits == 16 && (leading >= 51 && leading <= 55))
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((total_num_digits == 16 || total_num_digits == 13) && first_digit == 4)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+    return 0;
 }
 
-// returns leading digits
-int leading_digits(long long card, int num_leading_digits) {
-    // probably a better way to do this, but C is... pretty annoying ;)
-    // modulo doesn't appear to be working with long long data type
-    while (card > pow(10, num_leading_digits))
-    {
-        card /= 10;
-    }
-    return card;
+// returns the (int) digit at a particular position - zero-indexed, but starts from right, e.g, 543210
+int digit_at_position(long long num, int pos)
+{
+    long long int truncated_num = num / (long long int) pow(10, pos);
+    return (int) (truncated_num % 10);
 }
 
