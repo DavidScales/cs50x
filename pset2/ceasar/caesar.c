ed3a3a5185433522af67dc03fa6fb3627522b9c6
David Scales <davidsscales@gmail.com>
2018-01-18 04:24:09 +0000

    refactor&fix(pset1): abstract logic, fix null terminator bug

--- pset2/ceasar/caesar.c
+++ pset2/ceasar/caesar.c
@@ -3,13 +3,15 @@
 // Note: there is a bug such that every so often a capital "B" is added to the end
 // of the ciphertext. It's inconsistent. Assuming it has something to do with the
 // end of the array?
+// UPDATE - this is fixed - I had forgotten the \0 null terminator, so weird stuff
+// was happening at the end of strings
 
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
-char shifted(char letter, int key, int offset);
+char shifted(char letter, int key);
 
 int main(int argc, string argv[])
 {
@@ -21,29 +23,31 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    // validate CL input
+
+    int key = atoi(argv[1]);
+    if (key <= 0)
+    {
+        printf("Error: Incorrect input - key must be a positive integer\nUsage: ./ceasar key\n");
+        return 1;
+    }
+
     // get plaintext
 
     string plaintext = get_string("plaintext: ");
 
     // encrypt
 
-    int key = atoi(argv[1]); // assuming valid :O
-    int upperAsciiStart = 65;
-    int lowerAsciiStart = 97;
     int text_length = strlen(plaintext);
-    char ciphertext[text_length];
+    char ciphertext[text_length + 1]; // remember \0 null terminator!
     char letter;
 
-    for (int i = 0; i < text_length; i++)
+    for (int i = 0; i <= text_length; i++) // remember \0 null terminator!
     {
         letter = plaintext[i];
-        if (isupper(letter))
+        if (isalpha(letter))
         {
-            ciphertext[i] = shifted(letter, key, upperAsciiStart);
-        }
-        else if (islower(letter))
-        {
-            ciphertext[i] = shifted(letter, key, lowerAsciiStart);
+            ciphertext[i] = shifted(letter, key);
         }
         // else non-alphabetical - preserve
         else
@@ -59,8 +63,20 @@ int main(int argc, string argv[])
     return 0;
 }
 
-char shifted(char letter, int key, int offset)
+char shifted(char letter, int key)
 {
+    int upperAsciiStart = 65;
+    int lowerAsciiStart = 97;
+    int offset;
+    if (isupper(letter))
+    {
+        offset = upperAsciiStart;
+    }
+    else
+    {
+        offset = lowerAsciiStart;
+    }
+
     // convert from ascii to alphabetical index
     letter -= offset;
 
