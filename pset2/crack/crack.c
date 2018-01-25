3fce39537b1f71c9b70a6d8ab57ce43994a5653f
David Scales <davidsscales@gmail.com>
2018-01-25 01:58:51 +0000

    feat(pset2): crack - able to decrypt lowercase passwords

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -1,16 +1,27 @@
-// TODO - top level comment
+// Attempts to crack a hashed password (CL input arg) with brute force guess-and-check
 
-// TODO - review these
+// Usage ./crack hash
+
+// Notes:
+// * Assumes passwords are 5 alphabetical letters or less
+// * Assumes passwords are hashed with C's DES-based crypt function
+// * Guess-and-check simply loops through all possible combinations - it would of course
+//   be more performant to start with common passwords or dictionary words
+
+// Reference: https://docs.cs50.net/2018/x/psets/2/crack/crack.html
+
+// TODO - review headers
+// TODO - abstract into functions?
+// TODO - review strings vs char arrays vs points to char arrays
+
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#define _XOPEN_SOURCE // TODO - what is this?
+#define _XOPEN_SOURCE
 #include <unistd.h>
 #include <crypt.h>
 #include <math.h>
-
-void generate_plaintext(int length);
 
 int main(int argc, char *argv[])
 {
@@ -24,74 +35,67 @@ int main(int argc, char *argv[])
 
     // get hash & salt from input
 
-    // TODO - What's the deal with pointers again?
     char *hash = argv[1]; // assuming valid input :O
-    printf("hash: %s\n", hash);
-    char salt[] = { hash[0], hash[1], '\0' };
-    printf("salt: %s\n", salt);
+    char salt[] = { hash[0], hash[1], '\0' }; // salt for crypt is first two letters of hash
 
-    // original password completely alphabetical, 5 or less characters
-    // for a set of possible passwords (generate up to 5 alpha chars, case sensitive)
-        // hash each & compare to the input hash
+    // for passwords of length 1 through 5
 
-    // TESTING
-    // int comparison = strcmp(hash, crypt("hello", salt));
-    // printf("test comparision: %i\n", comparison);
+    for (int length = 1, max_length = 5; length <= max_length; length++)
+    {
 
-    // TESTING
-    generate_plaintext(3);
+        // create a starting word, e.g. "aaa"
 
-    // note - this would obviously be better if we started with common passwords or dictionary words
+        char word[length + 1];
+        for (int k = 0; k < length; k++)
+        {
+           word[k] = 'a';
+        }
+        word[length] = '\0';
 
-    // print password
-    // return 0
-    printf("IGNORE: %s\n", argv[1]);
+        // cycle through word possibilities
+        // doing this like you would code a UI timer - use a counter & convert the value to positions on a clock
+        // e.g., 0 == "aaa", 1 == "aab", 2 == "aac", etc.
+
+        int count;
+        int num_letters = 26;
+        int total = pow(num_letters, length);
+
+        // for each possible combination of letters e.g., "aaa" through "zzz"
+
+        for (int i = 0; i < total; i++)
+        {
+            count = i;
+
+            // assign letters to each position in the word
+
+            int value_at_pos;
+            int counts_for_pos;
+            int lowercase_offset = 97;
+
+            for (int pos = 0; pos < length; pos++)
+            {
+                counts_for_pos = pow(num_letters, (length - pos) - 1);
+                value_at_pos = count / counts_for_pos;
+                word[pos] = value_at_pos + lowercase_offset;
+                count -= counts_for_pos * value_at_pos;
+            }
+
+            // hash the word & compare agaisnt user supplied hash
+
+            if (strcmp(hash, crypt(word, salt)) == 0)
+            {
+                printf("%s\n", word);
+                return 0;
+            }
+
+            // duplicate for uppercase
+        }
+    }
+
+    printf("Couldn't decrypt hash :(\n");
+    return 2;
 }
-
-    // string hash = crypt(password, "aa");
-    // printf("hash:%s\n", hash);
 
     // TEMP hash aaPwJ9XL9Y99E maps to hello
 
-void generate_plaintext(int length)
-{
-    // generate starting word - TODO: how to abstract into function w/scope?
-
-    char word[length + 1];
-    for (int k = 0; k < length; k++)
-    {
-        word[k] = 'a';
-    }
-    word[length] = '\0';
-    printf("word: %s\n", word);
-    printf("length: %lu\n", strlen(word));
-
-    // cycle through word possibilities
-    // doing this like you would code a UI timer - use a counter & convert the value to positions on a clock
-
-    int lowercase_offset = 97;
-    int num_letters = 26;
-    int count = 0;
-    int total = pow(num_letters, length);
-    int value_at_pos;
-    int counts_for_pos;
-
-    for (int i = 0; i < total; i++)
-    {
-        count = i;
-
-        for (int pos = 0; pos < length; pos++)
-        {
-            counts_for_pos = pow(num_letters, (length - pos) - 1);
-            value_at_pos = count / counts_for_pos;
-            word[pos] = value_at_pos + lowercase_offset;
-            count -= counts_for_pos * value_at_pos;
-        }
-
-        printf("%i: %s\n", i, word);
-
-        // duplicate for uppercase
-    }
-}
-
-// 97 - 122 lowercase
+    // 97 - 122 lowercase
