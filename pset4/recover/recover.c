41e649f122c2c8811a4ca954afc0ab31a939b8fc
David Scales <davidsscales@gmail.com>
2018-02-15 01:45:01 +0000

    feat(pset4): recover - generate filenames

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -28,7 +28,11 @@ int main(int argc, char *argv[])
 
     // read 512B chunks into a buffer & check for JPEG signature at the beginning of chunk
 
-    int counter = 0;
+    int chunks = 0;
+    int image_count = 0;
+    // FILE *outptr = NULL;
+    int temp_started = 0;
+    char filename[8];
     BYTE buffer[512];
 
     while(fread(&buffer, 512, 1, inptr) != 0)
@@ -36,15 +40,39 @@ int main(int argc, char *argv[])
         // JPEG signature 0xff 0xd8 0xff 0xe*
         if (buffer[0] == 255 && buffer[1] == 216 && buffer[2] == 255 && buffer[3] >= 224 && buffer[3] <= 239)
         {
-            counter++;
-            printf("%i ", buffer[0]);
-            printf("%i ", buffer[1]);
-            printf("%i ", buffer[2]);
-            printf("%i ", buffer[3]);
-            printf("\n");
+            if (temp_started)
+            {
+                sprintf(filename, "%03i.jpg", image_count);
+                printf("%i bytes in %s\n", chunks * 512, filename);
+                chunks = 0;
+                image_count++;
+            }
+            else
+            {
+                temp_started = 1;
+                printf("first image found %i bytes into disk\n", chunks * 512);
+                chunks = 1;
+            }
+
+            // if outptr is isnt null, write previous file, open a new one
+            // if outptr is null (first image), just open a new one
+
+            // int *ptr = malloc();
+            // write to file
+            // free(ptr);
+
+            // FILE *outptr = fopen("a.jpg", "w");
+            // if (outptr == NULL)
+            // {
+            //     fprintf(stderr, "Could not open %s.\n", "a.jpg");
+            //     return 2;
+            // }
+            // fwrite(inptr, 512 * chunks, 1, outptr);
         }
+        chunks++;
     }
-    printf("%i potential JPEGs found\n", counter);
+    sprintf(filename, "%03i.jpg", image_count);
+    printf("%i bytes in %s\n", chunks * 512, filename);
 
     // Once I have the JPEG's ID'ed, I need to read files into a dynamic buffer
     // and then write them to files
